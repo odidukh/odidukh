563b706819e9258e5609ca826102bbb4e1fd81bc
odidukh <oles.didukh@gmail.com>
2022-06-25 23:25:21 +0300

    Style Skills page

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,56 +1,60 @@
 import React from "react";
 
+const SKILLS_SET = [
+    {
+        type: "Development Skills",
+        skills: [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux",
+            "Node",
+            "Express",
+            "MongoDB",
+        ],
+    },
+    {
+        type: "Project Managing Skills",
+        skills: ["Git", "Github", "Jira"],
+    },
+    {
+        type: "Personal Skills",
+        skills: ["Creative", "Analytical", "Team Player", "Adaptable"],
+    },
+];
+
 const Skills = () => {
     return (
         <div className="skillsPage">
             <div className="skillsDescription">
                 <h1>Skillset</h1>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Able to work with a variety of technologies and frameworks.
                     <br />
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Participated in a development of project of different sizes.
                     <br />
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Developed both SPAs and mobile apps.
                     <br />
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Proven ability to learn a custom frameworks and libraries.
                     <br />
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 </p>
             </div>
-            <div className="skillsTypesList">
-                <div className="skillsTypeListItem">
-                    <h3 className="skillsTypeListName">Development Skills</h3>
-                    <ul className="skillsList">
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <li>JavaScript</li>
-                        <li>React</li>
-                        <li>Redux</li>
-                        <li>Node</li>
-                        <li>Express</li>
-                        <li>MongoDB</li>
-                    </ul>
-                </div>
-                <div className="skillsTypeListItem">
-                    <h3 className="skillsTypeListName">
-                        Project Managing Skills
-                    </h3>
-                    <ul className="skillsList">
-                        <li>Git</li>
-                        <li>Github</li>
-                        <li>Jira</li>
-                    </ul>
-                </div>
-                <div className="skillsTypeListItem">
-                    <h3 className="skillsTypeListName">Personal Skills</h3>
-                    <ul className="skillsList">
-                        <li>Creative</li>
-                        <li>Analytical</li>
-                        <li>Team Player</li>
-                        <li>Tennis</li>
-                        <li>Motobike</li>
-                    </ul>
-                </div>
+            <div className="skillsBox">
+                {SKILLS_SET.map((skillSet) => {
+                    return (
+                        <div className="skillType" key={skillSet.type}>
+                            <h2 className="skillTypeName">{skillSet.type}</h2>
+                            <ul className="skillsList">
+                                {skillSet.skills.map((skill) => {
+                                    return (
+                                        <li className="skillItem">{skill}</li>
+                                    );
+                                })}
+                            </ul>
+                        </div>
+                    );
+                })}
             </div>
         </div>
     );
