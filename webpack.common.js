ae775d7aa17550427e5767757f05d87278663ddd
odidukh <oles.didukh@gmail.com>
2022-07-15 22:56:16 +0300

    Add favicon and title

--- webpack.common.js
+++ webpack.common.js
@@ -4,6 +4,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: "./public/index.html",
+            favicon: "./public/favicon.ico"
         }),
     ],
     module: {
