684fd55db98ba3e07a6461d6662a745c8883cc2e
odidukh <oles.didukh@gmail.com>
2022-07-18 23:07:28 +0300

    Edit About text

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,15 +14,11 @@ const About = () => {
             <div className="aboutMeTextBlock">
                 <h1>About Me</h1>
                 <p>
-                    Hi :) I am a front-end developer with 4 years of experience
-                    in
-                    <br />
-                    implementing various web solutions and creating mobile apps
-                    <br />
-                    using different frameworks and website designs. Strong
-                    creative
-                    <br />
-                    and analytical skills. Team player with an eye for detail.
+                    Hello. I'm a front-end developer with an extencive
+                    experience in an implemention of various web solutions and
+                    creation mobile apps using opensource and proprietary
+                    frameworks. Have strong creative and analytical skills. Team
+                    player with an eye for details.
                 </p>
                 <button className="letsTalkButton" onClick={handleClick}>
                     Let's Talk
